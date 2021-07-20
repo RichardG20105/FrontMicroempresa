75c9fdb1db27501c02816a0c0491bb5c95b3382c
RichardG20105 <richard20105@hotmail.com>
2021-07-20 17:32:33 -0500

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { PuntoventaListComponent } from './puntoventa-list/puntoventa-list.component';
-=======
 import { AdministradorListComponent } from './administrador-list/administrador-list.component';
->>>>>>> ff0b97753d3caa449af7917df2499b6a85ee076a
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    PuntoventaListComponent
-=======
+    PuntoventaListComponent,
     AdministradorListComponent
->>>>>>> ff0b97753d3caa449af7917df2499b6a85ee076a
   ],
   imports: [
     BrowserModule,
