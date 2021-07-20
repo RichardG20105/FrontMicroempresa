e16d4f485299aa8c0cab78ef16e4fd2491c8b884
drack25 <andynts25@gmail.com>
2021-07-20 17:02:36 -0500

    Puntoventa1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PuntoventaListComponent } from './puntoventa-list/puntoventa-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PuntoventaListComponent
   ],
   imports: [
     BrowserModule,
